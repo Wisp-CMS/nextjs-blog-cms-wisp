d23208454ef9c037646e4e6bf809d9203c252777
Raymond Yeh <ray@geek.sg>
2024-05-13 23:46:14 +0800

    chore: parameterized blog config

--- src/config.ts
+++ src/config.ts
@@ -1,17 +1,22 @@
 const buildConfig = () => {
   const blogId = process.env.NEXT_PUBLIC_BLOG_ID;
   if (!blogId) throw new Error("NEXT_PUBLIC_BLOG_ID is missing");
+  const name = process.env.NEXT_PUBLIC_BLOG_DISPLAY_NAME || "Travel.";
+  const copyright = process.env.NEXT_PUBLIC_BLOG_COPYRIGHT || "Samantha";
+  const defaultTitle =
+    process.env.NEXT_DEFAULT_METADATA_DEFAULT_TITLE || "Travel with Samantha";
+
   return {
     blog: {
-      name: "Travel.",
-      copyright: "Samantha",
+      name,
+      copyright,
       metadata: {
         title: {
-          absolute: "Travel with Samantha",
-          default: "Travel with Samantha",
-          template: "%s - Travel with Samantha",
+          absolute: defaultTitle,
+          default: defaultTitle,
+          template: `%s - ${defaultTitle}`,
         },
-      }
+      },
     },
     wisp: {
       blogId,
