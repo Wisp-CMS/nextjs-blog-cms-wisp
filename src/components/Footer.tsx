932ccc9d9896ed69fa0e3684c0a14ee99d62a8ca
Raymond Yeh <ray@geek.sg>
2024-07-22 18:00:41 +0800

    feat: powered by wisp

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -12,6 +12,13 @@ export const Footer: FunctionComponent = () => {
       <div className="text-sm text-muted-foreground">
         © {config.blog.copyright} {new Date().getFullYear()}
       </div>
+      <div className="text-xs text-muted-foreground">
+        <Link
+          href={`https://wisp.blog/?utm_source=next-js-template&utm_medium=web&utm_campaign=${config.baseUrl}`}
+        >
+          Blog powered by wisp
+        </Link>
+      </div>
       <div>
         <Link href="/rss">
           <Button variant="ghost" className="p-2">
