451e3fb833e8546e0dbdd8704ed1d04fa3e49f69
Raymond Yeh <ray@geek.sg>
2024-07-22 21:43:14 +0800

    chore: better footer placement

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,24 +8,33 @@ import { Button } from "./ui/button";
 
 export const Footer: FunctionComponent = () => {
   return (
-    <section className="flex items-center justify-between mt-8 md:mt-16 mb-12">
-      <div className="text-sm text-muted-foreground">
-        © {config.blog.copyright} {new Date().getFullYear()}
+    <section className="mt-8 md:mt-16 mb-12">
+      <div className="flex items-center justify-between">
+        <div className="text-sm text-muted-foreground">
+          © {config.blog.copyright} {new Date().getFullYear()}
+        </div>
+        <div className="text-xs text-muted-foreground hidden lg:block">
+          <Link
+            href={`https://wisp.blog/?utm_source=next-js-template&utm_medium=web&utm_campaign=${config.baseUrl}`}
+          >
+            Blog powered by wisp
+          </Link>
+        </div>
+        <div>
+          <Link href="/rss">
+            <Button variant="ghost" className="p-2">
+              <Rss className="w-4 h-4" />
+            </Button>
+          </Link>
+          <DarkModeToggle />
+        </div>
       </div>
-      <div className="text-xs text-muted-foreground">
+      <div className="text-xs text-muted-foreground lg:hidden">
         <Link
           href={`https://wisp.blog/?utm_source=next-js-template&utm_medium=web&utm_campaign=${config.baseUrl}`}
         >
           Blog powered by wisp
         </Link>
-      </div>
-      <div>
-        <Link href="/rss">
-          <Button variant="ghost" className="p-2">
-            <Rss className="w-4 h-4" />
-          </Button>
-        </Link>
-        <DarkModeToggle />
       </div>
     </section>
   );
