af7d3fb844f9cc9473f2891f69ae85f17adbfcfb
Raymond Yeh <ray@geek.sg>
2024-09-19 14:49:22 +0800

    feat: enabling video render

--- src/components/BlogPostContent.tsx
+++ src/components/BlogPostContent.tsx
@@ -1,55 +1,50 @@
 "use client";
 import { GetPostResult } from "@/lib/wisp";
 import Link from "next/link";
-import sanitize from "sanitize-html";
+import sanitize, { defaults } from "sanitize-html";
 
 export const PostContent = ({ content }: { content: string }) => {
+  const sanitizedContent = sanitize(content, {
+    allowedTags: [
+      "b",
+      "i",
+      "em",
+      "strong",
+      "a",
+      "img",
+      "h1",
+      "h2",
+      "h3",
+      "code",
+      "pre",
+      "p",
+      "li",
+      "ul",
+      "ol",
+      "blockquote",
+      // tables
+      "td",
+      "th",
+      "table",
+      "tr",
+      "tbody",
+      "thead",
+      "tfoot",
+      "small",
+      "div",
+      "iframe",
+    ],
+    allowedAttributes: {
+      ...defaults.allowedAttributes,
+      "*": ["style"],
+      iframe: ["src", "allowfullscreen", "style"],
+    },
+    allowedIframeHostnames: ["www.youtube.com", "www.youtube-nocookie.com"],
+  });
   return (
     <div
       className="blog-content mx-auto"
-      dangerouslySetInnerHTML={{
-        __html: sanitize(content, {
-          allowedTags: [
-            "b",
-            "i",
-            "em",
-            "strong",
-            "a",
-            "img",
-            "h1",
-            "h2",
-            "h3",
-            "code",
-            "pre",
-            "p",
-            "li",
-            "ul",
-            "ol",
-            "blockquote",
-            "td",
-            "th",
-            "table",
-            "tr",
-            "tbody",
-            "thead",
-            "tfoot",
-            "small",
-          ],
-          allowedAttributes: {
-            "*": ["style"],
-            a: ["href", "name", "target"],
-            img: [
-              "src",
-              "srcset",
-              "alt",
-              "title",
-              "width",
-              "height",
-              "loading",
-            ],
-          },
-        }),
-      }}
+      dangerouslySetInnerHTML={{ __html: sanitizedContent }}
     ></div>
   );
 };
